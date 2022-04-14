63c6b9fdcbda6794b11e487c0ad5c6b8d13d2a2f
VaibhaV246 <103430588+VaibhaV246@users.noreply.github.com>
2022-04-14 19:37:20 +0530

    Add files via upload

---
+++ Lab/weekend.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class weekend {
+
+	public static void main(String[] args) {
+	
+		Scanner no = new Scanner(System.in);
+		System.out.println("give the number =");
+		int day = no.nextInt();
+		
+		System.out.println(getDayName(day));
+		
+	}
+		public static String getDayName(int day) {
+			String dayName = "";
+			switch (day) {
+			case 1 : dayName = "Monday"; break;
+			 case 2: dayName = "Tuesday"; break;
+	            case 3: dayName = "Wednesday"; break;
+	            case 4: dayName = "Thursday"; break;
+	            case 5: dayName = "Friday"; break;
+	            case 6: dayName = "Saturday"; break;
+	            case 7: dayName = "Sunday"; break;
+	            default:dayName = "error(Invalid day range)";
+			}
+			return dayName;
+		}
+}
